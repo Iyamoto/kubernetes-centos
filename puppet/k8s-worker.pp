44908df326fc7e53e07e102ba5364a8ddb5bb9de
iyamoto <iyamoto@gmail.com>
2017-11-19 23:25:08 +0300

    config fixed for workers

--- puppet/k8s-worker.pp
+++ puppet/k8s-worker.pp
@@ -1,18 +1,10 @@
 
-# package { 'etcd':
-#   ensure  => installed,
-# } ->
-# file { '/etc/etcd/etcd.conf':
-#   ensure => 'file',
-#   source => '/vagrant/configs/etcd.conf',
-# } ->
-# service { 'etcd':
-#   enable => true,
-#   ensure => 'running',
-# }
-
 package { 'kubernetes':
   ensure  => installed,
+} ->
+file { '/etc/kubernetes/config':
+  ensure => 'file',
+  source => '/vagrant/configs/config',
 } ->
 file { '/etc/kubernetes/kubelet':
   ensure => 'file',
